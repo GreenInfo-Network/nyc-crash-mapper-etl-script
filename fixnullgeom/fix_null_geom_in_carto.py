65c52e2876097fdee0e899dba62b1b554acef6b8
Dan Rademacher <dan@greeninfo.org>
2023-09-22 15:28:42 -0700

    Add new years to scripts

--- fixnullgeom/fix_null_geom_in_carto.py
+++ fixnullgeom/fix_null_geom_in_carto.py
@@ -33,7 +33,7 @@ def run():
     # get our start & ending dates
     try:
         yyyymm = sys.argv[1]
-        if not re.match(r'^(2015|2016|2017|2018|2019|2020|2021|2022)\-(01|02|03|04|05|06|07|08|09|10|11|12)$', yyyymm):
+        if not re.match(r'^(2015|2016|2017|2018|2019|2020|2021|2022|2023|2024|2025)\-(01|02|03|04|05|06|07|08|09|10|11|12)$', yyyymm):
             raise IndexError
     except IndexError:
         print("Supply a YYYY-MM month. See docs for details.")
